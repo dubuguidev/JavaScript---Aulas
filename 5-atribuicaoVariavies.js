35055ec9ff012e04b4618ab7ebe2f0de19d6580a
Dubugui <gabrielbcostadev@gmail.com>
2025-08-21 11:12:04 -0300

    Add variable assignment, lists, and type conversion examples
    Expanded arithmetic and type conversion examples in 3-operacoesAritmeticas.js and 4-conversaoDeTipos.js. Added new files: 5-atribuicaoVariavies.js for variable assignment, 6-listas.js for array operations, and teste.html to load the list script.

---
+++ 5-atribuicaoVariavies.js
@@ -0,0 +1,18 @@
+console.log("Trabalhando com atribuição de variáveis");
+//const idade = 27;
+const primeiroNome = "Gabriel";
+const sobrenome = "Costa";
+
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0
+contador = contador +1
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto =2; 
+
+let idade; // declarando variável
+idade = 26; //atribuindo valor
+idade = idade +1; //atribui mais um ao número da variável
---
+++ 6-listas.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com Listas");
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+    `Curitiba`
+);
+
+listaDeDestinos.push(`Salinas`); //Adicionando um item na lista
+
+console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1);
